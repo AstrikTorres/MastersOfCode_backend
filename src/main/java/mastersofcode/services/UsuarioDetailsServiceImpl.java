8c6f989d44a740197af5a3298ae0b7b18cdb8b22
Astrik Torres <astriktorres@gmail.com>
2021-08-04 21:59:19 -0500

    Encriptado de contraseñas y JSON Web Token

---
+++ src/main/java/mastersofcode/services/UsuarioDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package mastersofcode.services;
+
+import mastersofcode.models.UsuariosModel;
+import mastersofcode.repositories.UsuarioRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import static java.util.Collections.emptyList;
+
+@Service
+public class UsuarioDetailsServiceImpl implements UserDetailsService {
+
+	private UsuarioRepository usuarioRepository;
+
+	public UsuarioDetailsServiceImpl(UsuarioRepository usuarioRepository) {
+		this.usuarioRepository = usuarioRepository;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		UsuariosModel usuariosModel = usuarioRepository.findByUsername(username);
+		if (usuariosModel == null) {
+			throw new UsernameNotFoundException(username);
+		}
+		return new User(usuariosModel.getUsername(), usuariosModel.getPassword(), emptyList());
+	}
+
+}
